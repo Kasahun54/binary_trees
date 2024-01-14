c92caa9482392a7f9f6c88325cb01acc52bc39c0
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2024-01-14 08:34:37 -0800

    remove white line

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Insert a node as a left-child 
- * 
+ * binary_tree_insert_left - Insert a node as a left-child
+ *
  * @parent: A pointer to insert left-child in
  * @value: New node
  *
  * Return:  NULL or  a pointer to new node
  *
- * Description: If the parent node already has a left child, 
- * 				the new node will replace it
+ * Description: If the parent node already has a left child,
+ * the new node will replace it
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
